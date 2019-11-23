b54173df896cfc530842ab0bd2e770562bee870d
feiersunshine <feiershine@outlook.com>
2019-11-23 20:15:43 +0800

    prac

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 #!/usr/bin/python3
-print("Hello, World!")
-print("My name is %s " % "zhangsan")
+def main():
+    print("Hello, World!")
+    print("My name is %s " % "zhangsan")
+    print()
+    a=[]
+    for i in range(3):
+        a.append(i)
+    print(a)
+
+if __name__ == "__main__":
+    main()
